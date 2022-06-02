e24cbad754eb44520f0cec5be76cf4c40ce4b749
Juan Escalada <juanescalada175@gmail.com>
2022-06-02 14:33:38 -0700

    Add json loader and update profile route

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 import os
+import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -13,5 +13,21 @@ def index():
 
 @app.route('/profile/<name>')
 def profile(name):
-    # todo access the JSON and look for the corresponding name, then pass it over as a variable to render it
-    return render_template('profile.html', name=name, url=os.getenv("URL"))
+    data = load_profiles_from_json('data.json')
+    info = data[name]
+    # todo check if name exists
+    print(info)
+    return render_template('profile.html', name=name, info=info, url=os.getenv("URL"))
+
+
+def load_profiles_from_json(filename) -> dict:
+    # Get the relative path for the JSON data
+    path = f'{os.getcwd()}/{filename}'
+    # Open the file and return its parsed contents
+    # UTF-8 encoding is used to parse apostrophes correctly
+    with open(path, "r", encoding='utf8') as file:
+        return json.load(file)
+
+
+def find_info_by_name(name: str):
+    print(name)
