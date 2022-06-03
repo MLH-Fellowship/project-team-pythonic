44c7d673629da9b9decc30e95734d54f11355083
Emily Lim <lim.emi@outlook.com>
2022-06-02 19:33:23 -0700

    Added coordinates to json file
    Added test marker onto map. Resized map.

--- app/static/scripts/maps.js
+++ app/static/scripts/maps.js
@@ -4,4 +4,8 @@ function profileMap() {
       zoom: 5,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(45.7315635, -122.6362767),
+        map: map,
+     });  
 }
