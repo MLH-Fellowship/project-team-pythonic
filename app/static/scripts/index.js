edd5723ac022d01c8ee9bf203d602dca6855c0b8
Juan Escalada <juanescalada175@gmail.com>
2022-06-03 15:59:51 -0700

    Add smart loading (execute automatically on mobile devices)

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -1,8 +1,17 @@
-let teamElement = document.getElementById("team-name");
-
-teamElement.addEventListener("mouseover", (event) => {
+function profilePopup() {
     let profiles = document.getElementsByClassName('profile_icon');
     for (let profile of profiles) {
         profile.classList.add("bubble-up");
     }
-})
+}
+
+function setup() {
+    let teamElement = document.getElementById("team-name");
+    let viewportWidth = window.innerWidth;
+    if (viewportWidth < 900) {
+        profilePopup();    
+    }
+    teamElement.addEventListener("mouseover", profilePopup);    
+}
+
+setup();
