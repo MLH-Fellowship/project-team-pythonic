c271ded1959682dd29c3e15ec0c7c61f8d58cde0
Emily Lim <lim.emi@outlook.com>
2022-06-03 11:52:48 -0700

    Added all markers for each map
    Each map contains all coordinates for specified profile. JSON coordinates and API key extracted with Jinja. Removed duplicate env files.

--- app/static/scripts/maps.js
+++ app/static/scripts/maps.js
@@ -1,11 +1,13 @@
 function profileMap() {
-    var mapProp = {
-      center: new google.maps.LatLng(45.7315635, -122.6362767),
-      zoom: 5,
+    var mapProp = { // map view begins zoomed out
+      center: new google.maps.LatLng(0, 0),
+      zoom: 1,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(45.7315635, -122.6362767),
-        map: map,
-     });  
+    for (let i = 0; i < countries.length; i++) { 
+      var marker = new google.maps.Marker({ // add a marker for each country visited, based on capital coordinates
+          position: new google.maps.LatLng(countries[i].lat, countries[i].long),
+          map: map,
+      });
+    }
 }
