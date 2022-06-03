0e37d4b460930d9de6403c98ed7e8703e111565a
Juan Escalada <juanescalada175@gmail.com>
2022-06-03 14:45:39 -0700

    Add bubble popup and simple floating animation

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -2,8 +2,7 @@ let teamElement = document.getElementById("team-name");
 
 teamElement.addEventListener("mouseover", (event) => {
     let profiles = document.getElementsByClassName('profile_icon');
-    console.log(profiles)
     for (let profile of profiles) {
-        profile.style.animation="bubble-up 2s infinite";
+        profile.classList.add("bubble-up");
     }
 })
