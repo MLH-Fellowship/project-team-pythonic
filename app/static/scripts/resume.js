4c9e0cb461602f37785d8d38090ffb190aafe7ab
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2022-06-14 21:28:01 -0400

    Revert "Added resume, css animations to resume page"
    This reverts commit 139f5fd98f6c538568c7a77ae0897931482384bb.

--- app/static/scripts/resume.js
+++
@@ -1,5 +0,0 @@
-function menuOnClick() {
-  document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
-}
