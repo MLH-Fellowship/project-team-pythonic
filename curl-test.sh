f892ace67142266d09742b853d38cfe594f982dc
Emily Lim <lim.emi@outlook.com>
2022-06-21 15:29:48 -0700

    Added MySQL database
    Created table in database for timeline posts. Created script file for checking POST and GET endpoints.

---
+++ curl-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# curl -X POST http://localhost:5000/api/timeline_post -d 'name=Emily&email=emily.lim@wsu.edu&content=Testing POST and GET endpoints!'
+
+curl "http://localhost:5000/api/timeline_post"
