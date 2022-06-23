545b1deb5445fbb5dcebbaf962ac1da3b8992716
Emily Lim <lim.emi@outlook.com>
2022-06-23 12:00:58 -0700

    Added MySQL code back. Added page for timeline
    Timeline page contains a form to submit POST requests and uses GET to display all posts beneath the form. Small changes to portfolio layout.

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,36 @@
 import os
 import json
-# import datetime
+import datetime
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-# from peewee import *
-# from playhouse.shortcuts import model_to_dict
+from peewee import *
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()  # Loads the environment variables from the .env file
 
 app = Flask(__name__)  # Initializes a Flask app
 
-# # connect to the portfolio database
-# mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-#         user=os.getenv("MYSQL_USER"),
-#         password=os.getenv("MYSQL_PASSWORD"),
-#         host=os.getenv("MYSQL_HOST"),
-#         port=3306
-# )
-# print(mydb)
+# connect to the portfolio database
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+)
+print(mydb)
 
-# # ORM model for timeline posts in the portfolio
-# class TimelinePost(Model):
-#     name = CharField()
-#     email = CharField()
-#     content = TextField()
-#     created_at = DateTimeField(default=datetime.datetime.now)
+# ORM model for timeline posts in the portfolio
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
 
-#     class Meta:
-#         database = mydb
+    class Meta:
+        database = mydb
 
-# mydb.connect()
-# mydb.create_tables([TimelinePost]) # create database table for posts
+mydb.connect()
+mydb.create_tables([TimelinePost]) # create database table for posts
 
 os.getenv("API_KEY")  # Obtains the value of the .env variable containing the Google Maps API key
 
@@ -80,24 +80,32 @@ def load_profiles_from_json(filename) -> dict:
     with open(path, "r", encoding='utf8') as file:
         return json.load(file)
 
-# ## create pair of endpoints to save and retrieve posts:
-# # add POST route to add a timeline post
-# @app.route('/api/timeline_post', methods=['POST'])
-# def post_time_line_post():
-#     name = request.form['name']
-#     email = request.form['email']
-#     content = request.form['content']
-#     timeline_post = TimelinePost.create(name=name, email=email, content=content)
+## create pair of endpoints to save and retrieve posts:
+# add POST route to add a timeline post
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
-#     return model_to_dict(timeline_post) # helper method
+    return model_to_dict(timeline_post) # helper method
 
-# # add GET endpoint to retrieve all timeline posts
-# @app.route('/api/timeline_post', methods=['GET'])
-# def get_time_line_post():
-#     return {
-#         'timeline-posts': [
-#             model_to_dict(p)
-#             # newest timeline posts are returned at the top
-#             for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-#         ]
-#     }
+# add GET endpoint to retrieve all timeline posts
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline-posts': [
+            model_to_dict(p)
+            # newest timeline posts are returned at the top
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+# route to the timeline page
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
