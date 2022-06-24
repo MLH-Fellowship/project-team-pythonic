1c4f90582e411636d27f00303c1f7d70895a37d6
Emily Lim <lim.emi@outlook.com>
2022-06-23 18:18:02 -0700

    Updated redeploy-site.sh for systemd
    Portfolio now redeploys as a service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 # Script to automate deployment of the portfolio website.
-# url: http://emilyllim.duckdns.org:5000/
-
-# kill all existing tmux sessions
-tmux kill-session
 
 # cd into project folder
 cd /root/mlh-fellowship-portfolio
 
-# make sure the git repository has the latest changes from main
+# make sure git repository has the latest changes from main
 git fetch && git reset origin/main --hard
 
 # enter Python virtual environment
@@ -18,6 +14,6 @@ source python3-virtualenv/bin/activate
 # install Python dependencies
 pip install -r requirements.txt
 
-# start new detached tmux session, enter the Python virtual environment,
-# cd into the project directory, and then start up the Flask server
-tmux new-session -d -s my-session "source python3-virtualenv/bin/activate && cd /root/mlh-fellowship-portfolio && flask run --host=0.0.0.0"
+# restart myportfolio service
+systemctl daemon-reload
+systemctl restart myportfolio
