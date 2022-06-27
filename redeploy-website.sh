5805c81fed012d6cec5d4e0fd652568bdf8fe29b
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2022-06-27 11:50:50 -0400

    Switched from tmux to myportfolio.service

--- redeploy-website.sh
+++ redeploy-website.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tmux kill-server
+
 
 cd portfolio-website
 
@@ -8,9 +8,6 @@ git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-
-tmux new -s portfolio
-tmux send -t portfolio: $'cd portfolio-website' C-m
-tmux send -t portfolio: $'source python3-virtualenv/bin/activate' C-m
-tmux send -t portfolio: $'export FLASK_ENV=development' C-m
-tmux send -t portfolio: $'flask run --host=0.0.0.0' C-m
+ 
+systemctl daemon-reload
+systemctl restart myportfolio
