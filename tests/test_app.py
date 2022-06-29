734c45b501c4a1812361ef6039e4a16406bf11a0
EmilieYZhang <emilieyzh@gmail.com>
2022-06-29 19:31:09 -0400

    Added unit tests for API endpoints and error handling

---
+++ tests/test_app.py
@@ -0,0 +1,83 @@
+# tests/test_app.py
+
+import unittest
+import os
+os.environ['TESTING'] = 'true'
+
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+    
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<title>Emily Lim</title>" in html
+        # add more tests relating to the homepage
+        assert '<meta charset="utf-8" />' in html
+        assert '<meta property="og:title" content="Personal Portfolio" />' in html
+        assert '<meta property="og:description" content="My Personal Portfolio" />' in html
+        assert '<h1 id="herofont"><span id="pythonic_color">EMILY </span>LIM</h1>' in html
+        assert '<h2 id="herosubfont">Aspiring Production Engineer</h2>' in html
+    
+    def test_timeline(self):
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline-posts" in json
+        assert len(json["timeline-posts"]) == 0
+
+        # Add more tests relating to the POST GET api
+        # Testing POST request
+        first_post = self.client.post('/api/timeline_post', data={'name':'Tom Doe', 'email':'tom@example.com', 'content':'Hello world, I\'m Tom!'})
+        assert first_post.status_code == 200
+
+        second_post = self.client.post('/api/timeline_post', data={'name':'Jenn Doe', 'email':'jenn@example.com', 'content':'Hello world, I\'m Jenn!'})
+        assert first_post.status_code == 200
+
+        # Testing GET request
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline-posts" in json
+        assert len(json["timeline-posts"]) == 2
+
+        assert json["timeline-posts"][0]['id'] == 2
+        assert json["timeline-posts"][0]['name'] == 'Jenn Doe'
+
+        assert json["timeline-posts"][1]['id'] == 1
+        assert json["timeline-posts"][1]['name'] == 'Tom Doe'
+    
+    # Testing timeline page
+    def test_timelinepage(self):
+        response = self.client.get("/timeline")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<title>Document</title>" in html
+        assert '<button type="submit">Submit</button>' in html
+        assert '<input name="name" placeholder="Name" type="text">' in html
+        assert '<input name="email" placeholder="Email" type="text">' in html
+        assert '<textarea name="content" placeholder="Content" type="text"></textarea>' in html
+
+    def test_malformed_timeline_post(self):
+        # POST request missing name
+        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
+
+        # POST request with empty content
+        response = self.client.post("/api/timeline_post", data={"name" : "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", data={"name" : "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
---
+++ tests/test_db.py
@@ -0,0 +1,53 @@
+# test_db.py
+
+import unittest
+from peewee import *
+
+from app import TimelinePost
+from playhouse.shortcuts import model_to_dict
+
+MODELS = [ TimelinePost ]
+
+# use an in-memory SQLite for tests.
+test_db = SqliteDatabase(':memory')
+
+class TestTimelinePost(unittest.TestCase):
+    def setUp(self):
+        # Bind model classes to test db. Since we have a complete list of
+        # all models, we do not need to recursively bind dependencies.
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+
+        test_db.connect()
+        test_db.create_tables(MODELS)
+    
+    def tearDown(self):
+        # Note strictly necessary since SQLite in-memory databases only live
+        # for the duration of the connection, and in the next step we close
+        # the connection... but a good practice all the same.
+        test_db.drop_tables(MODELS)
+
+        # Close connection to db.
+        test_db.close()
+    
+    def test_timeline_post(self):
+        # Create 2 timeline posts.
+        first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+        assert first_post.id == 1
+
+        second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+        assert second_post.id == 2
+
+        # Get timeline posts and assert that they are correct
+        timeline_posts = [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+
+        # Assert that length of timeline is as expected: 2 posts
+        assert len(timeline_posts) == 2
+
+        # Assert that first and second posts have correct name and id
+        post_1 = TimelinePost.get(TimelinePost.id == 1)
+        assert post_1.name == 'John Doe'
+        post_2 = TimelinePost.get(TimelinePost.id == 2)
+        assert post_2.name == 'Jane Doe'
