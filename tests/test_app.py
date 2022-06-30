70a68892ea6edd2e779c334091245d19a6250bc8
mmiah <mmiah@bu.edu>
2022-06-30 19:33:21 -0400

    added tests for GET and POST for /api/timeline and to test timeline page

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,7 +17,7 @@ class AppTestCase(unittest.TestCase):
         assert "<div class=\"hero_header\">" in html 
         assert "<h1 id=\"herofont\"><span id=\"pythonic_color\">Hanna </span>Gersten</h1>" in html 
         assert "<h2 id=\"herosubfont\">Aspiring Software Engineer</h2>" in html 
-        # TODO Add more tests relating to the home page 
+        
     
     def test_timeline(self): 
         response = self.client.get("/api/timeline_post")
@@ -25,6 +25,21 @@ class AppTestCase(unittest.TestCase):
         assert response.is_json 
         json = response.get_json()
         assert "timeline_posts" in json 
-        assert len(json["timeline_posts"]) == 0 #28
-        # TODO Add more tests relating to the /api/timeline_post GET and POST apis
-        # TODO Add more tests relating to the timeline page 
+        assert len(json["timeline_posts"]) == 0 
+
+        # Tests relating to the /api/timeline_post GET and POST 
+        first_post = self.client.post("/api/timeline_post/name=John Doe&email=john@example.com&content=Hi!My name is John")
+        second_post = self.client.post("/api/timeline_post/name=Jane Doe&email=jane@example.com&content=Hi!My name is Jane")
+        
+        response2 = self.client.get("/api/timeline_post")
+        assert response2.is_json
+        json2 = response2.get_json() 
+        print (json2)
+        assert "timeline_posts" in json2
+        assert len(json["timeline_posts"]) == 2 
+        
+        # Tests relating to the timeline page 
+        timeline_response = self.client.get('/timeline')
+        assert timeline_response.status_code == 200
+        html = timeline_response.get_data(as_text=True)
+        assert "<title>Timeline</title>" in html 
