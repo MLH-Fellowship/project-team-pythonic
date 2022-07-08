e2ea4a3c5544408f72d90ea63a30f5514a168163
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2022-07-08 14:31:56 -0400

    Updated redeploy script to accomodate Docker

--- redeploy-website.sh
+++ redeploy-website.sh
@@ -6,8 +6,5 @@ cd portfolio-website
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
- 
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
